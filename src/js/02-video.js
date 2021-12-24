6d1a9dafb33b44446b722ae51df03c93605ea1ba
Nazariy Bakush <nazkamaz@gmail.com>
2021-12-24 16:57:25 +0200

    hw8/t2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+function playerSetTimeUpdate(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(playerSetTimeUpdate, 1000));

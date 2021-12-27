a5a2078eb86fb32e2e2cd8f3e9a1471bff47061d
Nazariy Bakush <nazkamaz@gmail.com>
2021-12-27 17:45:00 +0200

    Update 03-feedback.js
    hw8.3 LT

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,44 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const inputEmail = document.querySelector('.feedback-form input');
-const textareaText = document.querySelector('.feedback-form textarea');
-const submitButton = document.querySelector('.feedback-form button');
+const elements = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('.feedback-form input'),
+  textareaText: document.querySelector('.feedback-form textarea'),
+  submitButton: document.querySelector('.feedback-form button'),
+};
 const localStorageKey = 'feedback-form-state';
-populateEmail();
-populateTextarea();
+const storageObj = {
+  email: '',
+  message: '',
+};
+basicStorage();
+// populateEmail();
+// populateTextarea();
 
-inputEmail.addEventListener('input', throttle(onEmailInput, 500));
-textareaText.addEventListener('input', throttle(onTextareaInput, 500));
-form.addEventListener('submit', onFormSubmit);
+elements.form.addEventListener('submit', onFormSubmit);
+elements.inputEmail.addEventListener('input', throttle(onEmailInput, 500));
+elements.textareaText.addEventListener('input', throttle(onTextareaInput, 500));
+
+function basicStorage() {
+  const currLocStor = localStorage.getItem(localStorageKey);
+
+  const parsLocStor = JSON.parse(currLocStor);
+  //   console.log(parsLocStor.email);
+  //   console.log(parsLocStor.message);
+  if (!parsLocStor) {
+    localStorage.setItem(localStorageKey, JSON.stringify(storageObj));
+  } else {
+    populateEmail();
+    populateTextarea();
+  }
+}
 
 function onFormSubmit(event) {
   event.preventDefault();
+  if (elements.inputEmail.value === '' || elements.textareaText.value === '') {
+    window.alert('one or both of the fields are empty!');
+    return;
+  }
   const sendData = localStorage.getItem(localStorageKey);
   console.log(JSON.parse(sendData));
   event.currentTarget.reset();
@@ -22,42 +47,57 @@ function onFormSubmit(event) {
 
 function populateEmail() {
   const locStorCurr = localStorage.getItem(localStorageKey);
+  //   console.log(locStorCurr);
   const parsLocStorCurr = JSON.parse(locStorCurr);
-  if (parsLocStorCurr && parsLocStorCurr.email) {
-    const savedEmail = parsLocStorCurr.email;
-    if (savedEmail) {
-      inputEmail.value = savedEmail;
-    }
+  //   console.log(parsLocStorCurr);
+
+  if (parsLocStorCurr.email) {
+    elements.inputEmail.value = parsLocStorCurr.email;
   }
 }
 
 function populateTextarea() {
   const locStorCurr = localStorage.getItem(localStorageKey);
   const parsLocStorCurr = JSON.parse(locStorCurr);
-  if (parsLocStorCurr && parsLocStorCurr.message) {
-    const savedTextarea = parsLocStorCurr.message;
-    if (savedTextarea) {
-      textareaText.value = savedTextarea;
-    }
+  if (parsLocStorCurr.message) {
+    elements.textareaText.value = parsLocStorCurr.message;
   }
 }
 
 function onEmailInput(event) {
   const emailValue = event.target.value;
-  const locStorVal = {
-    email: emailValue,
-    message: '',
-  };
-  localStorage.setItem(localStorageKey, JSON.stringify(locStorVal));
+  const currLocStor = localStorage.getItem(localStorageKey);
+  //   console.log(currLocStor);
+  const parsLocStor = JSON.parse(currLocStor);
+  //   console.log(parsLocStor);
+
+  if (!parsLocStor) {
+    localStorage.setItem(localStorageKey, JSON.stringify(storageObj));
+
+    const locStor = localStorage.getItem(localStorageKey);
+    const parsLocStor = JSON.parse(locStor);
+    parsLocStor.email = emailValue;
+    localStorage.setItem(localStorageKey, JSON.stringify(parsLocStor));
+  } else {
+    parsLocStor.email = emailValue;
+    localStorage.setItem(localStorageKey, JSON.stringify(parsLocStor));
+  }
 }
 
 function onTextareaInput(event) {
-  const textareaValue = event.target.value;
-
+  const currTextareaValue = event.target.value;
   const currentLocStor = localStorage.getItem(localStorageKey);
-  const parsCurentLocStor = JSON.parse(currentLocStor);
+  //   console.log(currentLocStor);
+  const parsCurrentLocStor = JSON.parse(currentLocStor);
 
-  parsCurentLocStor.message = textareaValue;
-
-  const finalLocStor = localStorage.setItem(localStorageKey, JSON.stringify(parsCurentLocStor));
+  if (!parsCurrentLocStor) {
+    localStorage.setItem(localStorageKey, JSON.stringify(storageObj));
+    const locStor = localStorage.getItem(localStorageKey);
+    const parsLocStor = JSON.parse(locStor);
+    parsLocStor.message = currTextareaValue;
+    localStorage.setItem(localStorageKey, JSON.stringify(parsLocStor));
+  } else {
+    parsCurrentLocStor.message = currTextareaValue;
+    localStorage.setItem(localStorageKey, JSON.stringify(parsCurrentLocStor));
+  }
 }
